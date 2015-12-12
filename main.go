627441185f06907a49a2e5b54c85d34cecb37b25
Integralist <mark.mcdx@gmail.com>
2015-12-12 15:41:12 +0000

    Remove use of multiple authors (let's face it; it'll only ever be me)

--- main.go
+++ main.go
@@ -12,24 +12,6 @@ import (
 
 const url = "http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html"
 
-var authorList []cli.Author
-
-func author(name, email string) cli.Author {
-	return cli.Author{
-		Name:  name,
-		Email: email,
-	}
-}
-
-func authors() []cli.Author {
-	authorList = append(
-		authorList,
-		author("Mark McDonnell", "mark.mcdx@gmail.com"),
-	)
-
-	return authorList
-}
-
 func commandNotFound(c *cli.Context, command string) {
 	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
 	os.Exit(2)
@@ -39,7 +21,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Sainsbury Scraper"
 	app.Version = "0.0.1"
-	app.Authors = authors()
+	app.Author = "Mark McDonnell"
 	app.Usage = "CLI tool for scraping contents from Sainsbury website"
 	app.Action = process
 
